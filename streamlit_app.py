59fb30e7c9f0c839e295e95639e707669ddf9efb
RAKVTM <r.abukhadra@gmail.com>
2026-04-13 17:08:14 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,11 @@ st.title("🎴 TCG Portfolio AI Client")
 # 1. Establish Connection
 conn = st.connection("gsheets", type=GSheetsConnection)
 
-# 2. Read the Pokémon Raw tab
-df = conn.read(worksheet="Pokemon_Raw")
+# 2. Read the Data (Notice we put the URL directly here now)
+# REPLACE the URL below with your actual Google Sheet URL
+url = "https://docs.google.com/spreadsheets/d/1sJviZ9ZkfRsQpONd08qRD4ly_CVXJyayhqx0ecCSTd0/edit#gid=0"
+
+df = conn.read(spreadsheet=url, worksheet="Pokemon_Raw")
 
 # 3. Display the Data
 st.subheader("Current Inventory: Pokémon Raw")
